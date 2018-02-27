eb95d509be9a41c1c5af106a5255ab7e6c7a850c
thefapinstructor <thefapinstructor@gmail.com>
2018-02-27 08:16:18 -0500

    Added code-monkey to error page

--- src/Containers/ErrorBoundary.js
+++ src/Containers/ErrorBoundary.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Raven from "raven-js";
+import monkey from "images/monkey.gif"
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { error: null, info: null };
+
+    Raven.config("https://75a8117570924a2eb142da7e40a17968@sentry.io/204785", {
+      release: "2.0.0",
+      maxUrlLength: 2000
+    }).install();
+    console.log('raven config')
   }
 
   componentDidCatch(error, info) {
     this.setState({ error, info });
+    Raven.captureException(error);
   }
 
   render() {
@@ -18,6 +27,8 @@ class ErrorBoundary extends React.Component {
       return (
         <div>
           <h1>Something went wrong.</h1>
+          <h2>The error has been logged and we've dispatched the code monkeys</h2>
+          <img src={monkey} alt="code monkey" />
         </div>
       );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Raven from "raven-js";
 import registerServiceWorker from "./registerServiceWorker";
 import "./polyfills/endsWith";
 import "./polyfills/performance";
@@ -10,10 +9,6 @@ import Root from "./Components/Root";
 import configureStore from "./configureStore";
 
 const store = configureStore();
-
-Raven.config("https://75a8117570924a2eb142da7e40a17968@sentry.io/204785", {
-  release: "2.0.0"
-}).install();
 
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 registerServiceWorker();
