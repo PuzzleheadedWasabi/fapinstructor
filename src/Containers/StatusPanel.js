94ea038949bdbfe725255443c2b068c587da34d5
thefapinstructor <thefapinstructor@gmail.com>
2018-03-01 23:31:53 -0500

    Finished styling the statuspanel

--- src/Containers/StatusPanel.js
+++ src/Containers/StatusPanel.js
@@ -1,53 +1,78 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
+import Button from "material-ui/Button";
 import connect from "hoc/connect";
 import elapsedGameTime from "game/utils/elapsedGameTime";
 import { round } from "utils/math";
 import { GripStrengthString } from "game/enums/GripStrength";
+import logo from "images/logo.svg";
 
 const styles = theme => ({
   root: {
-    opacity: "0.6",
-    color: "white",
-    backgroundColor: "black",
     position: "absolute",
     zIndex: 99,
-    padding: 10
+    padding: 5,
+    background: "rgba(0, 0, 0, 0.6)"
   },
-  label: {
-    opacity: "unset",
-    width: 150,
-    display: "inline-block"
+  header: {
+    display: "flex",
+    alignItems: "center",
+    color: "white"
   },
-  labelValue: {
-    opacity: "unset",
-    display: "inline-block",
-    fontWeight: "bold"
+  labels: {
+    display: "flex"
   }
 });
 
-const Label = ({ classes, name }) => (
-  <Typography className={classes.label} color="secondary" variant="body2">
-    {name}
-  </Typography>
-);
-
-const LabelValue = ({ classes, value }) => (
-  <Typography className={classes.labelValue} color="secondary" variant="body2">
+const Label = ({ value }) => (
+  <Typography color="secondary" variant="body2">
     {value}
   </Typography>
 );
 
-const StatusPanel = ({ classes, game: { strokeSpeed, gripStrength } }) => (
-  <div className={classes.root}>
-    <Label classes={classes} name="Elapsed Time (min)" />
-    <LabelValue classes={classes} value={elapsedGameTime("minutes")} />
-    <Label classes={classes} name="Stroke Speed (s)" />
-    <LabelValue classes={classes} value={round(strokeSpeed, 2)} />
-    <Label classes={classes} name="Stroke Grip" />
-    <LabelValue classes={classes} value={GripStrengthString[gripStrength]} />
-  </div>
-);
+class StatusPanel extends React.Component {
+  state = {
+    open: true
+  };
+
+  render() {
+    const { open } = this.state;
+    const { classes, game: { strokeSpeed, gripStrength } } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <div
+          className={classes.header}
+          onClick={() => this.setState({ open: !open })}
+        >
+          <Button
+            color="inherit"
+            style={{ textTransform: "none", width: "100%" }}
+          >
+            <img style={{ width: 25, marginRight: 10 }} src={logo} alt="Logo" />
+            <Typography color="inherit" variant="body2">
+              Fap Instructor
+            </Typography>
+          </Button>
+        </div>
+        {open && (
+          <div className={classes.labels}>
+            <div style={{ marginRight: 10 }}>
+              <Label value="Elapsed Time (min)" />
+              <Label value="Stroke Speed (s)" />
+              <Label value="Stroke Grip" />
+            </div>
+            <div>
+              <Label value={elapsedGameTime("minutes")} />
+              <Label value={round(strokeSpeed, 2)} />
+              <Label value={GripStrengthString[gripStrength]} />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(connect(StatusPanel));
